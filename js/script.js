f2642bb19881ace9ad771b850d8e4a1ceb4e132a
Amur <amyrkatyk@gmail.com>
2024-01-19 11:25:58 +0500

    тест скрипта смены картинок по свайпу на главной

--- js/script.js
+++ js/script.js
@@ -170,6 +170,9 @@ zoomImg.addEventListener('mouseleave', () => {
 
 import { productCards, changeColorOnCard, changeImgOnHover, returnFirstImg } from './productCards.js';
 
+let xDown = null;
+let yDown = null;
+
 productCards.forEach(card => {
     card.addEventListener('change', (event) => {
         changeColorOnCard(event, card);
@@ -179,6 +182,60 @@ productCards.forEach(card => {
     progressBarItems.forEach(item => {
         const length = progressBarItems.length;
         item.style.width = 100 / length + '%';
+    });
+
+    const productImgBox = card.querySelector('.product-card__img-box');
+
+    productImgBox.addEventListener('touchstart', (evt) => {
+        xDown = evt.touches[0].clientX;
+        yDown = evt.touches[0].clientY;
+    });
+
+    productImgBox.addEventListener('touchmove', (evt) => {
+        if (!xDown || !yDown) {
+            return;
+        }
+        let xUp = evt.touches[0].clientX;
+        let yUp = evt.touches[0].clientY;
+
+        let xDiff = xDown - xUp;
+        let yDiff = yDown - yUp;
+
+        const progressBarContainer = productImgBox.querySelector('.thumbs-progressbar');
+        const currentItem = progressBarContainer.querySelector('.current');
+        const nextItem = currentItem.nextElementSibling;
+        const prevItem = currentItem.previousElementSibling;
+        const progressBarArr = Array.from(progressBarItems);
+        let currentIndex = progressBarArr.indexOf(currentItem);
+
+
+        // немного поясню здесь. Тут берутся модули движения по оси абсцисс и ординат (почему модули? потому что если движение сделано влево или вниз, то его показатель будет отрицательным) и сравнивается, чего было больше: движения по абсциссам или ординатам. Нужно это для того, чтобы, если пользователь провел вправо, но немного наискосок вниз, сработал именно коллбэк для движения вправо, а ни как-то иначе.
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+            if (xDiff > 0) {
+                if (nextItem) {
+                    hideOtherItems(progressBarItems, 'current');
+                    addClass(nextItem, 'current');
+                    currentIndex += 1;
+                    const thumb = thumbs[currentIndex];
+                    const thumbImg = thumb.querySelector('img');
+                    const thumbImgSrc = thumbImg.getAttribute('src');
+                    productImg.setAttribute('src', thumbImgSrc)
+                }
+            } else if (xDiff < 0) {
+                if (prevItem) {
+                    hideOtherItems(progressBarItems, 'current');
+                    addClass(prevItem, 'current')
+                    currentIndex -= 1;
+                    const thumb = thumbs[currentIndex];
+                    const thumbImg = thumb.querySelector('img');
+                    const thumbImgSrc = thumbImg.getAttribute('src');
+                    productImg.setAttribute('src', thumbImgSrc)
+                }
+            }
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;
     });
 
     /* Смена картинки при наведении */
